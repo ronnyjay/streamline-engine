6984b79586fc0d4df1af5949ab880a3e43d5a4c4
Aaron Matthews <aaronmat3@gmail.com>
2024-05-11 14:52:07 -0400

    corrected stbi image loading

--- src/engine/texture/texture.cpp
+++ src/engine/texture/texture.cpp
@@ -7,87 +7,39 @@
 
 using namespace engine;
 
-std::vector<unsigned char> loadBMP(const std::filesystem::path &filepath, int &width, int &height)
-{
-    std::ifstream file(filepath, std::ios::binary);
-    if (!file.is_open())
-    {
-        throw std::runtime_error("Failed to load texture: " + filepath.string());
-    }
-
-    // Read BMP header
-    file.seekg(18);
-    file.read(reinterpret_cast<char *>(&width), 4);
-    file.read(reinterpret_cast<char *>(&height), 4);
-
-    int num_pixels = width * height;
-    std::vector<unsigned char> data(num_pixels * 3); // 3 channels for RGB
-
-    file.seekg(54); // Skip to pixel data
-    file.read(reinterpret_cast<char *>(data.data()), data.size());
-
-    // BMP uses BGR format, so swap red and blue channels
-    for (int i = 0; i < num_pixels * 3; i += 3)
-    {
-        unsigned char temp = data[i];
-        data[i] = data[i + 2];
-        data[i + 2] = temp;
-    }
-
-    return data;
-}
-
 Texture::Texture(const std::filesystem::path &image_path)
 {
-    std::vector<unsigned char> data;
+    unsigned char *data = nullptr;
+    data = stbi_load(image_path.c_str(), &m_width, &m_height, &m_channels, 0); // desired channels to 0 to load with original number of channels
 
-    if (image_path.extension() == ".bmp")
+    if (!data)
     {
-        m_channels = 3;
-        data = loadBMP(image_path, m_width, m_height);
-        glGenTextures(1, &m_id);
-        glBindTexture(GL_TEXTURE_2D, m_id);
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_width, m_height, 0, GL_RGB, GL_UNSIGNED_BYTE, data.data());
-        glGenerateMipmap(GL_TEXTURE_2D);
+        throw std::runtime_error("Failed to load texture: " + image_path.string());
     }
-    else
+
+    GLint internalformat;
+    switch (m_channels)
     {
-        unsigned char *data = nullptr;
-        data = stbi_load(image_path.c_str(), &m_width, &m_height, &m_channels, 4);
-
-        if (!data)
-        {
-            throw std::runtime_error("Failed to load texture: " + image_path.string());
-        }
-
-        GLint internalformat;
-        switch (m_channels)
-        {
-        case 3:
-            internalformat = GL_RGB;
-            break;
-        case 4:
-            internalformat = GL_RGBA;
-            break;
-        default:
-            throw std::runtime_error("Unhandled channel size for texture: " + image_path.string());
-        }
-
-        glGenTextures(1, &m_id);
-        glBindTexture(GL_TEXTURE_2D, m_id);
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-        glTexImage2D(GL_TEXTURE_2D, 0, internalformat, m_width, m_height, 0, internalformat, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-
-        stbi_image_free(data);
+    case 3:
+        internalformat = GL_RGB;
+        break;
+    case 4:
+        internalformat = GL_RGBA;
+        break;
+    default:
+        throw std::runtime_error("Unhandled channel size for texture: " + image_path.string());
     }
+
+    glGenTextures(1, &m_id);
+    glBindTexture(GL_TEXTURE_2D, m_id);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, internalformat, m_width, m_height, 0, internalformat, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(data);
 }
 
 void Texture::bind()
