44839d0ee1c63c8704d4b27799ef4c16607e9c98
Aaron <aaronmat3@gmail.com>
2024-11-08 10:47:27 -0700

    lots of code

--- src/main.cpp
+++ src/main.cpp
@@ -49,14 +49,10 @@ void benchmark_glm()
 
 void benchmark_slm()
 {
-    // clang-format off
-    union { __m128 simd_vector; float coefficients[4]; };
-    // clang-format on
-
-    slm::vec4 vector = slm::vec4(1.0f, 2.0f, 3.0f, 4.0f);
+    Vector<4, float> vector = slm::vec4(1.0f, 2.0f, 3.0f, 4.0f);
 
     // clang-format off
-    slm::mat4 matrix = slm::mat4(
+    Matrix<4, 4, float> matrix = slm::mat4(
         1.0f, 5.0f, 9.0f, 13.0f,
         2.0f, 6.0f, 10.0f, 14.0f,
         3.0f, 7.0f, 11.0f, 15.0f,
@@ -64,37 +60,64 @@ void benchmark_slm()
     );
     // clang-format on
 
-    coefficients[0] = vector.x;
-    coefficients[1] = vector.y;
-    coefficients[2] = vector.z;
-    coefficients[3] = vector.w;
-
-    _mm_setr_ps(vector.x, vector.y, vector.z, vector.w);
-
-    __m128 simd_matrix_columns[4];
-    simd_matrix_columns[0] = _mm_setr_ps(matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3]);
-    simd_matrix_columns[1] = _mm_setr_ps(matrix[1][0], matrix[1][1], matrix[1][2], matrix[1][3]);
-    simd_matrix_columns[2] = _mm_setr_ps(matrix[2][0], matrix[2][1], matrix[2][2], matrix[2][3]);
-    simd_matrix_columns[3] = _mm_setr_ps(matrix[3][0], matrix[3][1], matrix[3][2], matrix[3][3]);
-
     auto start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 1000000; i++)
     {
-
-        // clang-format off
-        __m128 simd_result = _mm_fmadd_ps(_mm_set1_ps(coefficients[0]), simd_matrix_columns[0],
-            _mm_fmadd_ps(_mm_set1_ps(coefficients[1]), simd_matrix_columns[1],
-            _mm_fmadd_ps(_mm_set1_ps(coefficients[2]), simd_matrix_columns[2],
-                    _mm_mul_ps(_mm_set1_ps(coefficients[3]), simd_matrix_columns[3])
-                )
-            )
-        );
-        // clang-format on
+        slm::vec4 result = matrix * vector;
     }
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
     std::cout << "slm multiplication took: " << duration.count() << " ms." << std::endl;
 }
+
+// void benchmark_slm()
+// {
+//     // clang-format off
+//     union { __m128 simd_vector; float coefficients[4]; };
+//     // clang-format on
+
+//     slm::vec4 vector = slm::vec4(1.0f, 2.0f, 3.0f, 4.0f);
+
+//     // clang-format off
+//     slm::mat4 matrix = slm::mat4(
+//         1.0f, 5.0f, 9.0f, 13.0f,
+//         2.0f, 6.0f, 10.0f, 14.0f,
+//         3.0f, 7.0f, 11.0f, 15.0f,
+//         4.0f, 8.0f, 12.0f, 16.0f
+//     );
+//     // clang-format on
+
+//     coefficients[0] = vector.x;
+//     coefficients[1] = vector.y;
+//     coefficients[2] = vector.z;
+//     coefficients[3] = vector.w;
+
+//     _mm_setr_ps(vector.x, vector.y, vector.z, vector.w);
+
+//     __m128 simd_matrix_columns[4];
+//     simd_matrix_columns[0] = _mm_setr_ps(matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3]);
+//     simd_matrix_columns[1] = _mm_setr_ps(matrix[1][0], matrix[1][1], matrix[1][2], matrix[1][3]);
+//     simd_matrix_columns[2] = _mm_setr_ps(matrix[2][0], matrix[2][1], matrix[2][2], matrix[2][3]);
+//     simd_matrix_columns[3] = _mm_setr_ps(matrix[3][0], matrix[3][1], matrix[3][2], matrix[3][3]);
+
+//     auto start = std::chrono::high_resolution_clock::now();
+//     for (int i = 0; i < 1000000; i++)
+//     {
+
+//         // clang-format off
+//         __m128 simd_result = _mm_fmadd_ps(_mm_set1_ps(coefficients[0]), simd_matrix_columns[0],
+//             _mm_fmadd_ps(_mm_set1_ps(coefficients[1]), simd_matrix_columns[1],
+//             _mm_fmadd_ps(_mm_set1_ps(coefficients[2]), simd_matrix_columns[2],
+//                     _mm_mul_ps(_mm_set1_ps(coefficients[3]), simd_matrix_columns[3])
+//                 )
+//             )
+//         );
+//         // clang-format on
+//     }
+//     auto end = std::chrono::high_resolution_clock::now();
+//     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+//     std::cout << "slm multiplication took: " << duration.count() << " ms." << std::endl;
+// }
 
 int main(void)
 {
--- src/math/matrix.hpp
+++ src/math/matrix.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#if defined(__AVX__) || defined(__FMA__)
+#include <immintrin.h>
+#endif
+
 #include <xmmintrin.h> // SSE intrinsics
 
 #include "vector.hpp"
@@ -152,12 +156,21 @@ class Matrix
         return m;
     }
 
-    /**
+#if defined(__AVX__) || defined(__FMA__)
+    // clang-format off
+
+     /**
      * @brief
      *
      * @param other
      * @return Vector<M, T>
      */
+    template <
+        std::size_t X = M, std::size_t Y = N, typename U = T,
+        typename = std::enable_if_t<
+            !((X == 4 && Y == 4 && std::is_same<U, float>::value) ||
+              (X == 3 && Y == 3 && std::is_same<U, float>::value) ||
+              (X == 2 && Y == 2 && std::is_same<U, float>::value))>>
     Vector<M, T> operator*(const Vector<N, T> &other)
     {
         Vector<M, T> v;
@@ -170,6 +183,85 @@ class Matrix
         }
         return v;
     }
+
+    template <typename = std::enable_if<M == 2 && N == 2>>
+    Vector<2, T> operator*(const Vector<2, T> &other)
+    {
+        union { __m128 simd_vector; float  coefficients[2]; };
+
+        coefficients[0] = other[0];
+        coefficients[1] = other[1];
+
+        _mm_setr_ps(0.0f, 0.0f, coefficients[0], coefficients[1]);
+
+        __m128 simd_matrix_columns[2];
+        simd_matrix_columns[0] = _mm_setr_ps(0.0f, 0.0f, data[0][0], data[0][1]);
+        simd_matrix_columns[1] = _mm_setr_ps(0.0f, 0.0f, data[1][0], data[1][1]);
+
+        __m128 simd_result = _mm_fmadd_ps(_mm_set1_ps(coefficients[0]), simd_matrix_columns[0],
+             _mm_mul_ps(_mm_set1_ps(coefficients[1]), simd_matrix_columns[1]));
+
+        float simd_results[4];
+        _mm_store_ps(simd_results, simd_result);
+
+        return Vector<2, T>(simd_results[0], simd_results[1]);
+    }
+
+    template <typename = std::enable_if<M == 3 && N == 3>>
+    Vector<3, T> operator*(const Vector<3, T> &other)
+    {
+        union { __m128 simd_vector; float  coefficients[3]; };
+    }
+
+    template <typename U = T, std::enable_if_t<M == 4 && N == 4 && std::is_same<U, float>::value, int> = 0>
+    Vector<4, T> operator*(const Vector<4, T> &other)
+    {
+       union { __m128 simd_vector; float  coefficients[4]; };
+
+        coefficients[0] = other[0];
+        coefficients[1] = other[1];
+        coefficients[2] = other[2];
+        coefficients[3] = other[3];
+
+        _mm_setr_ps( coefficients[0], coefficients[1], coefficients[2], coefficients[3]);
+
+        __m128 simd_matrix_columns[4];
+        simd_matrix_columns[0] = _mm_setr_ps(data[0][0], data[0][1], data[0][2], data[0][3]);
+        simd_matrix_columns[1] = _mm_setr_ps(data[1][0], data[1][1], data[1][2], data[1][3]);
+        simd_matrix_columns[2] = _mm_setr_ps(data[2][0], data[2][1], data[2][2], data[2][3]);
+        simd_matrix_columns[3] = _mm_setr_ps(data[3][0], data[3][1], data[3][2], data[3][3]);
+
+        __m128 simd_result = _mm_fmadd_ps(_mm_set1_ps(coefficients[0]), simd_matrix_columns[0],
+            _mm_fmadd_ps(_mm_set1_ps(coefficients[1]), simd_matrix_columns[1],
+            _mm_fmadd_ps(_mm_set1_ps(coefficients[2]), simd_matrix_columns[2],
+                    _mm_mul_ps(_mm_set1_ps(coefficients[3]), simd_matrix_columns[3])
+                )
+            )
+        );
+
+        float simd_results[4];
+        _mm_store_ps(simd_results, simd_result);
+
+        return Vector<4, T>(simd_results[0], simd_results[1], simd_results[2], simd_results[3]);
+    }
+    //clang-format on
+
+#else
+
+    Vector<M, T> operator*(const Vector<N, T> &other)
+    {
+        Vector<M, T> v;
+        for (size_t i = 0; i < M; i++)
+        {
+            for (size_t j = 0; j < N; j++)
+            {
+                v[i] += data[i][j] * other[j];
+            }
+        }
+        return v;
+    }
+
+#endif
 
   private:
     std::array<Vector<N, T>, M> data;
