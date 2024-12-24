845569e69f6c7c7dc7aef8f77e44b12119b023ef
Aaron Matthews <aaronmat3@gmail.com>
2024-12-24 12:58:16 -0500

    restart

--- src/engine/Entity/Entity.cpp
+++
@@ -1,9 +0,0 @@
-#include <engine/Entity/Entity.hpp>
-
-using namespace engine;
-
-Entity::Entity(entt::entity handle, Scene *scene)
-    : m_Handle(handle)
-    , m_Scene(scene)
-{
-}
--- src/engine/stb/stb_image.cpp
+++
@@ -1,2 +0,0 @@
-#define STB_IMAGE_IMPLEMENTATION
-#include <engine/stb/stb_image.hpp>
